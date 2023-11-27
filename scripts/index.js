7426d67109c9d5c813acbe8c7e295c3035b74894
Roweens <enderman2011@gmail.com>
2023-11-27 23:01:57 +0300

    fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,23 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 const cardList = document.querySelector('.places__list');
 
-function addCard(card, deleteCb) {
-    const { imageUrl, title } = card;
+function createCard({ imageUrl, title }, deleteCb) {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     const deleteButton = cardElement.querySelector('.card__delete-button');
 
     cardElement.querySelector('.card__image').src = imageUrl;
+    cardElement.querySelector('.card__image').alt = title;
     cardElement.querySelector('.card__title').textContent = title;
 
-    deleteButton.addEventListener('click', () => deleteCb(title));
+    deleteButton.addEventListener('click', () => deleteCb(cardElement));
 
-    cardList.append(cardElement);
+    return cardElement;
 }
 
-function deleteCard(title) {
-    const selectedCard = Array.from(cardList.children).find((card) =>
-        card.innerHTML.includes(title)
-    );
-    selectedCard.remove();
+function deleteCard(card) {
+    card.remove();
 }
 
 initialCards.forEach((card) => {
-    addCard({ imageUrl: card.link, title: card.name }, deleteCard);
+    cardList.append(createCard({ imageUrl: card.link, title: card.name }, deleteCard));
 });
